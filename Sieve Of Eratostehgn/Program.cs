f06207469281dfb7d9780237c37b634ce7a279d1
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-01-28 07:23:29 +0200

    Add files via upload

---
+++ Sieve Of Eratostehgn/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Sieve_Of_Eratostehgn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num=int.Parse(Console.ReadLine()); 
+
+            bool [] primes= new bool[num+1];
+
+            for (int i = 2; i <=num; i++)
+            {
+                primes[i] = true;
+            }
+            
+        }
+    }
+}
