3771a66a857b6657fcb6e67f2c8020c5fc637f4c
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-01-28 06:53:34 +0200

    Add files via upload

---
+++ Array Sort/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Array_Sort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num=Console.ReadLine().Split(',').Select(int.Parse).ToArray();
+
+            int index = 0;
+            for (int i = 0; i <num.Length-index; i++)
+            {
+                int temp = 0;
+                
+                if (num[i] == 0)
+                {
+                    temp = num[(num.Length - 1)-index];
+                    num[(num.Length - 1)-index] = num[i];
+                    num[i]=temp;
+                    index++;
+                }
+                
+            }
+            Console.WriteLine(string.Join(',', num));
+        }
+    }
+}
---
+++ Arrays- Telerik More/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Arrays__Telerik_More
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] number=Console.ReadLine().Split(',').Select(int.Parse).ToArray();
+
+            string[] strange=new string[number.Length];
+
+            int index = 0;
+            for (int i = 0; i <number.Length; i++)
+            {
+                if (number[i] < 0) 
+                {
+                    strange[index] =number[i].ToString();
+                    index++;  
+                }
+            }
+            for (int i = 0; i <number.Length; i++)
+            {
+                if (number[i] == 0)
+                {
+                    strange[index] = number[i].ToString();
+                    index++;
+                }
+               
+            }
+           
+            for (int i = 0; i < number.Length; i++)
+            {
+                if (number[i] > 0)
+                {
+                    strange[index] = number[i].ToString();
+                    index++;
+                }
+               
+            }
+            Console.WriteLine(string.Join(',',strange));
+        }
+    }
+}
