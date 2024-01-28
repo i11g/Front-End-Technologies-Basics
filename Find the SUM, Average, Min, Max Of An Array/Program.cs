004274ccbdfd346ae03a17fc12e84eafe1154bed
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-01-28 07:27:32 +0200

    Add files via upload

---
+++ Find the SUM, Average, Min, Max Of An Array/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Find_the_SUM__Average__Min__Max_Of_An_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num=int.Parse(Console.ReadLine());
+
+            int[] numbers=new int[num];
+
+            int sum = 0;
+            int max=int.MinValue; int min=int.MaxValue;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+                sum += numbers[i];
+                if (numbers[i] > max)
+                {
+                    max = numbers[i];
+                }
+                if (numbers[i] < min)
+                {
+                    min = numbers[i];
+                }
+            }
+            Console.WriteLine(sum);
+            Console.WriteLine(max);
+                Console.WriteLine(min);
+            Console.WriteLine($"{sum/num:f2}");
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[numbers.Length-1]);
+        }
+    }
+}
