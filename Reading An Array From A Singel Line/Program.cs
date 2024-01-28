f7fe69598b14b87dfd1e3d486ea9792b6b6d235e
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-01-28 07:25:56 +0200

    Add files via upload

---
+++ Reading An Array From A Singel Line/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Reading_An_Array_From_A_Singel_Line
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string value=Console.ReadLine();
+
+            string[] items = value.Split(' ');
+
+            int[] arr = new int[items.Length];
+
+            for (int i = 0; i < items.Length; i++)
+            {
+                arr[i] = int.Parse(items[i]);
+            }
+            foreach (int ar in arr)
+            {
+                Console.WriteLine(ar);
+            }
+        }
+    }
+}
---
+++ Reading Arrays/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Reading_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] days =
+            {
+                "Monday",
+                "Tuesday",
+                "Wendesday"
+
+            };
+            Console.WriteLine(days[0]);
+            days[days.Length-1] = "tuk";
+
+            for (int i = 0; i <days.Length; i++)
+            {
+                Console.WriteLine(days[i]);
+            }
+        }
+    }
+}
