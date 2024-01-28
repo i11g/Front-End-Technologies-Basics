7b7dcb352bf07b03df189626369fe067a027ccc5
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-01-28 07:28:32 +0200

    Add files via upload

---
+++ Creating Arrays 2/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Creating_Arrays_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = new string[5];
+            array[0] = "Monday";
+             
+            array[2] = "Wednesday";
+            array[3] = "Thursday";
+            array[4] = "Friday";
+
+            array[array.Length - 1] = "posleden";
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
---
+++ Day Of Week/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Day_Of_Week
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] daysOfWeek = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+
+            int num = int.Parse(Console.ReadLine());
+
+            if (num >= 1 && num <= 7)
+            {
+                Console.WriteLine(daysOfWeek[num - 1]);
+            }
+            else 
+            {
+                Console.WriteLine("Invalid Day!");
+            }
+
+            int[] numbers=new int[10];
+
+            for (int i = 0; i <numbers.Length; i++)
+            {
+                numbers[i] = 1;
+            }
+                 
+        }
+    }
+}
