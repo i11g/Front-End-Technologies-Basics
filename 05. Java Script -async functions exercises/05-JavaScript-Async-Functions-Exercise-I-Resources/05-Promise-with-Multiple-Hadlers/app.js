44b1d8f4b099e425773a573604c48ae763084dd2
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-05-24 09:27:33 +0300

    files changed

--- 05. Java Script -async functions exercises/05-JavaScript-Async-Functions-Exercise-I-Resources/05-Promise-with-Multiple-Hadlers/app.js
+++ 05. Java Script -async functions exercises/05-JavaScript-Async-Functions-Exercise-I-Resources/05-Promise-with-Multiple-Hadlers/app.js
@@ -1,3 +1,11 @@
 function promiseWithMultipleHandlers() {
-    // TODO
+    let promise=new Promise(function(resolve,reject){
+        setTimeout(() => {
+            console.log ("Hello World");
+            console.log ("Hello World");
+        }, 2000);
+    })
+    promise.then(function(result){
+        console.log(result);
+    })
 }
