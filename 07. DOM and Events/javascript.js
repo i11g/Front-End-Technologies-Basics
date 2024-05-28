8a6b275ad821e71f229343d234b59c0cd08c56d9
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-05-28 19:19:08 +0300

    initial commit

--- 07. DOM and Events/javascript.js
+++ 07. DOM and Events/javascript.js
@@ -1,4 +1,11 @@
 
 function showAlert() {
     alert("It's me")
-}
+} 
+
+let div=document.getElementById("newdiv")
+console.log(div.innerHTML)
+div.innerHTML="New title" 
+
+let paragraphs=document.getElementsByTagName("p")
+console.log(paragraphs)
