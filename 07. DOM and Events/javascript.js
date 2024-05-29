e61e15b5e091835bc5ac12617aa5006e18f9eeab
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-05-29 15:49:18 +0300

    files changed

--- 07. DOM and Events/javascript.js
+++ 07. DOM and Events/javascript.js
@@ -1,11 +1,14 @@
 
-function showAlert() {
-    alert("It's me")
-} 
+// function showAlert() {
+//     alert("It's me")
+// } 
 
-let div=document.getElementById("newdiv")
-console.log(div.innerHTML)
-div.innerHTML="New title" 
+// //let div=document.getElementById("newdiv")
+// //console.log(div.innerHTML)
+// //div.innerHTML="New title" 
 
-let paragraphs=document.getElementsByTagName("p")
-console.log(paragraphs)
+// //let paragraphs=document.getElementsByTagName("p")
+// //console.log(paragraphs)
+
+// let paragraph=document.querySelectorAll("p");
+// console.log(paragraph[0])
