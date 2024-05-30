26bdb4c63b97838c66af5f3563e90d2cf406ecd8
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-05-30 15:52:01 +0300

    files changed

--- 08. DOM and Events exercise/09-DOM-and-Events-Exercise-Resources/03-Accordion/accordion.js
+++ 08. DOM and Events exercise/09-DOM-and-Events-Exercise-Resources/03-Accordion/accordion.js
@@ -1,3 +1,15 @@
 function toggle() {
-    // TODO...
+    let button=document.getElementsByClassName("button")[0];
+    let extraText=document.getElementById("extra");
+
+    let buttontext=button.textContent; 
+
+    if(buttontext=="More") {
+        extraText.style.display="block"
+        button.textContent="Less"
+    }
+    else {
+        extraText.style.display="none"
+        button.textContent="More"
+    }
 }
