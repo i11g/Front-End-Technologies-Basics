4fdd3c2bea079d71b7c4f46db9a000c8698252be
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-05-30 06:58:13 +0300

    files changed

--- 07. DOM and Events/events.js
+++ 07. DOM and Events/events.js
@@ -1,5 +1,32 @@
-const button=document.getElementById("newbutton")
+//add eventListener
 
-button.addEventListener("click", function (event) {
-    console.log("newEvent")
+// let button=document.getElementById("newbutton")
+
+// button.addEventListener("click", function (event) {
+//     console.log("newEvent")
+// })
+
+//add and remove eventListener 
+
+// let button=document.getElementById("newbutton")
+
+// button.addEventListener("click", showAlert) 
+
+// function showAlert() {
+//     alert("It's me")
+// }
+
+// setTimeout(function(){
+//   button.removeEventListener("click", showAlert)
+// }, 5000)
+
+//mouse over event 
+
+let button=document.getElementById("newbutton") 
+button.addEventListener("mouseover", function(event){
+    button.style.background="red"
 })
+
+button.addEventListener("mouseout", function(event){
+    button.style.background="blue"
+}) 
