43d0f0c8f8fc575d68f25418a99c4782c2dc601e
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-05-30 13:57:52 +0300

    files changed

--- 08. DOM and Events exercise/09-DOM-and-Events-Exercise-Resources/01-Subtraction/subtract.js
+++ 08. DOM and Events exercise/09-DOM-and-Events-Exercise-Resources/01-Subtraction/subtract.js
@@ -1,3 +1,10 @@
 function subtract() {
-    // TODO...
+    let firstElement=document.getElementById("firstNumber")
+    let secondElement=document.getElementById("secondNumber")
+    let resultDiv=document.getElementById("result")
+    
+    result=Number(firstElement.value)-Number(secondElement.value)
+    
+    resultDiv.textContent=result;
+
 } 
