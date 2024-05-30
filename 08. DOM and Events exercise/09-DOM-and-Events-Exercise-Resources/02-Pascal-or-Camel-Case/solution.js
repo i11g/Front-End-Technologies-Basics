875f093daf64b974ab2cc2d6158d4d5e3c9672fd
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-05-30 15:22:47 +0300

    files changed

--- 08. DOM and Events exercise/09-DOM-and-Events-Exercise-Resources/02-Pascal-or-Camel-Case/solution.js
+++ 08. DOM and Events exercise/09-DOM-and-Events-Exercise-Resources/02-Pascal-or-Camel-Case/solution.js
@@ -1,3 +1,28 @@
 function solve() {
-  // TODO...
+   let textInput=document.getElementById("text").value.toLowerCase();
+   let namingConvention=document.getElementById("naming-convention").value;
+   let resultField=document.getElementById("result") 
+   
+   let splittedtext=textInput.split(' ')
+
+   let resulttext="";
+
+   if(namingConvention=="Camel Case") {
+    resulttext +=splittedtext[0];
+    for (let i=1; i<splittedtext.length; i++) {
+      resulttext +=splittedtext[i][0].toUpperCase() + splittedtext[i].slice(1,splittedtext[i].length)
+    }
+  }
+   else if (namingConvention=="Pascal Case"){
+    for (let i=0; i<splittedtext.length; i++) {
+      resulttext+=splittedtext[i][0].toUpperCase() + splittedtext[i].slice(1,splittedtext[i].length)
+    }
+
+   }
+   else {
+    resultField.textContent="Error"
+   }
+
+   resultField.textContent=resulttext;
+
 }
