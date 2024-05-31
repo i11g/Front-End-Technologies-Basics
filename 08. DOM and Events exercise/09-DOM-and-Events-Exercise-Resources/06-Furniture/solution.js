e0eb3eaecef0fe8b01dbcc38e6f9b4f1de0eed4c
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-05-31 06:59:07 +0300

    files changed

--- 08. DOM and Events exercise/09-DOM-and-Events-Exercise-Resources/06-Furniture/solution.js
+++ 08. DOM and Events exercise/09-DOM-and-Events-Exercise-Resources/06-Furniture/solution.js
@@ -1,3 +1,50 @@
 function solve() {
     const [input, output]=document.getElementsByTagName("textarea")
+    const [generateButton, buyButton]=document.getElementsByTagName("button") 
+    const tableBody=document.getElementsByTagName("tbody")[0] 
+
+    generateButton.addEventListener("click", generateRow)
+    //buyButton.addEventListener('click', buyItems) 
+
+    function generateRow () {
+     let data=JSON.parse(input.value)
+
+     for (let i=0; i<data.length; i++) {
+
+      let tableRow=document.createElement('tr')
+
+      let tableDataI=document.createElement('td')
+      let tableImage=document.createElement('img')
+      tableImage.src=data[i].img
+
+      tableDataI.appendChild(tableImage)
+      tableRow.appendChild(tableDataI)
+
+      let tableDataName=document.createElement('td')
+      let tableDataparagraph=document.createElement('p')
+      tableDataparagraph.textContent=data[i].name
+      
+      tableDataName.appendChild(tableDataparagraph)
+      tableRow.appendChild(tableDataName)
+
+      let tablePrice=document.createElement('td')
+      let tablePriceparagraph=document.createElement('p')
+      tablePriceparagraph.textContent=data[i].price
+
+      tablePrice.appendChild(tablePriceparagraph)
+      tableRow.appendChild(tablePrice)
+
+      let tableDecfactor=document.createElement('td')
+      let tableDecfactorparagraph=document.createElement('p')
+      tableDecfactorparagraph.textContent=data[i].decFactor
+
+      tableDecfactor.appendChild(tableDecfactorparagraph)
+      tableRow.appendChild(tableDecfactor)
+
+      tableBody.appendChild(tableRow)
+     }
+
+    }
+
+
 }
