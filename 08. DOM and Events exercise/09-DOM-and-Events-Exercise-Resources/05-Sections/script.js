74dcd84adc2445f3e7abd0aa25c5a0fc5d9a638f
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-06-01 16:03:07 +0300

    files changed

--- 08. DOM and Events exercise/09-DOM-and-Events-Exercise-Resources/05-Sections/script.js
+++ 08. DOM and Events exercise/09-DOM-and-Events-Exercise-Resources/05-Sections/script.js
@@ -1,3 +1,19 @@
 function create(words) {
-   
+    let divContent=document.getElementById("content") 
+
+    for (let i = 0; i < words.length; i++) {
+        
+        let elementDiv=document.createElement("div")
+        let elementParagraph=document.createElement('p')
+        elementParagraph.textContent=words[i]
+        elementParagraph.style.display='none' 
+
+        elementDiv.appendChild(elementParagraph)
+        divContent.appendChild(elementDiv)
+
+        elementDiv.addEventListener('click', function() {
+            elementParagraph.style.display=''
+        })
+
+    }
 }
