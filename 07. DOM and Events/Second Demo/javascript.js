92da20a9306e545d5dad23b86602967661d8b016
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-06-01 19:17:39 +0300

    files changed

--- 07. DOM and Events/Second Demo/javascript.js
+++ 07. DOM and Events/Second Demo/javascript.js
@@ -1,8 +1,10 @@
 const movieList=document.getElementById("movie-list") 
 
+//create new element
 let moviItem=document.createElement("li")
 moviItem.textContent="Man of Steel" 
 
+//add the element with delay
 setTimeout(function(){
     movieList.appendChild(moviItem)
 }, 5000)
