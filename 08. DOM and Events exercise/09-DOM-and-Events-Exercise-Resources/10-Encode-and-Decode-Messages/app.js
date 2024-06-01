553fbbf6a6788dc6f5fbcf26d6713c31ddb913b3
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-06-01 08:24:06 +0300

    files changed

--- 08. DOM and Events exercise/09-DOM-and-Events-Exercise-Resources/10-Encode-and-Decode-Messages/app.js
+++ 08. DOM and Events exercise/09-DOM-and-Events-Exercise-Resources/10-Encode-and-Decode-Messages/app.js
@@ -1,3 +1,35 @@
 function encodeAndDecodeMessages() {
-    // TODO...
+      let buttons=document.getElementsByTagName("button")
+      let encodeButton=buttons[0]
+      let decodeButton=buttons[1] 
+
+      let textAreas=document.getElementsByTagName("textarea")
+      let encodeTextArea=textAreas[0]
+      let decodeTextArea=textAreas[1] 
+
+      function codeText (text, formatedFunction) {
+
+        return text.split('').map(formatedFunction).join('')
+      }
+
+      function encode (char) {
+        return String.fromCharCode(char.charCodeAt()+1)
+      }
+
+      function decode(char) {
+        return String.fromCharCode(char.charCodeAt()-1)
+      }
+
+      function encodetext () {
+        let encodeText=encodeTextArea.value
+        decodeTextArea.value=codeText(encodeText, encode)
+        encodeTextArea.value=""
+      }
+
+      function decodeText() {
+        decodeTextArea.value=codeText(decodeTextArea.value, decode)
+      }
+
+      encodeButton.addEventListener('click', encodetext)
+      decodeButton.addEventListener('click', decodeText)
 }
