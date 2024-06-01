1542ad2f98c02e59377584853bc9125321d3fae0
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-06-01 09:56:45 +0300

    files changed

--- 08. DOM and Events exercise/09-DOM-and-Events-Exercise-Resources/09-Fill-Dropdown/dropdown.js
+++ 08. DOM and Events exercise/09-DOM-and-Events-Exercise-Resources/09-Fill-Dropdown/dropdown.js
@@ -1,3 +1,18 @@
 function addItem() {
-    let 
+
+    let text=document.getElementById("newItemText" )
+    let valueE=document.getElementById("newItemValue")
+    let selectItem=document.getElementById("menu") 
+
+    let optionElement=document.createElement("option") 
+
+    optionElement.textContent=text.value
+    optionElement.value=valueE.value
+
+    
+   selectItem.appendChild(optionElement)
+
+   text.value=""
+   valueE.value=""
+    
 }
