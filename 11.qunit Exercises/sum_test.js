f4a681589f742a16aa23816d3ea9148d3e452d42
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-06-10 14:05:44 +0300

    files changed

--- 11.qunit Exercises/sum_test.js
+++
@@ -1,7 +0,0 @@
-const {sum} =require("./tests_functions.js") 
-
-QUnit.module("sum functions tests", ()=> {
-    QUnit.test("Adding two positive numbers", function(assert) {
-        assert.equal(sum(2,3), 5, "Adding two positive numbers")
-    })
-})
---
+++ 11.qunit_exercises/sum_test.js
@@ -0,0 +1,14 @@
+const {sum} =require("./tests_functions.js") 
+
+QUnit.module("sum functions tests", ()=> {
+    QUnit.test("Adding two positive numbers", function(assert) {
+        assert.equal(sum(2,3), 5, "Adding two positive numbers")
+    })
+    QUnit.test("Adding two negative numbers", function(assert) {
+        assert.equal(sum(-2,-3), -5, "Adding two negative numbers")
+    })
+
+    QUnit.test("Adding two floating point numbers", function (assert) {
+        assert.equal(sum(0.3, 0.4), 0.7, "Adding two floating point numbers")
+    })
+})
