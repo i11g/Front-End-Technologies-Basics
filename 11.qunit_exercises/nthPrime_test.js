a86613865c31dfa2473ba350229e05b3af89af8f
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-06-16 14:08:50 +0300

    files changed

--- 11.qunit_exercises/nthPrime_test.js
+++ 11.qunit_exercises/nthPrime_test.js
@@ -1,7 +1,13 @@
-const {ntPrimre} =require("./tests_functions.js") 
+const {nthPrime} = require("./tests_functions.js") 
 
-QUnit.module("nthPrime function tests", function(){
+QUnit.module("nthPrime function tests", function() {
     QUnit.test("1th Prime", function(assert){
-        assert.equal(ntPrimre(10),  )
+        assert.equal (nthPrime(1), 2, "1th Prime" )
+    }),
+    QUnit.test("second parameter", function(assert) {
+        assert.equal(nthPrime(5), 11,"second parameter" )
+    })
+    QUnit.test("third parameter", function(assert){
+        assert.equal(nthPrime(11),31,"third parameter")
     })
 })
