bfa1284b47bfc4555ed3346c2e394e7295a6d5ab
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-06-16 15:45:08 +0300

    files changed

--- 12. Asynchronous Testing with QUnit/async_test_functions.js
+++ 12. Asynchronous Testing with QUnit/async_test_functions.js
@@ -1,13 +1,11 @@
 async function fetchData(url) {
-    let result= await fetch(url).then(
-        response=>{
+    let result= await fetch(url)
+    .then(response=>{
             if(response.ok) {
                 return response.json()
             }
-        }
-        
-    )
-    .then(data=>data)
+        })     
+    //.then(data=>data)
     .catch(error=>`${error.message}`)
      
     return result
@@ -15,4 +13,9 @@ async function fetchData(url) {
 
 function fake_delays(miliseconds) {
      return new Promise(resolve=>setTimeout(resolve, miliseconds))
+} 
+
+module.exports={
+    fetchData,
+    fake_delays
 }
