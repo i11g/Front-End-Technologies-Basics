2c6d33f337c8d9b2eecce7507f13f1772b74147a
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-06-21 06:53:45 +0300

    files changed

--- 14. Exam Preparation/20.Exam-Preparation-I-Resources/02.JS-App-For-Testing/tests/QUnit_tests/integration.test.js
+++ 14. Exam Preparation/20.Exam-Preparation-I-Resources/02.JS-App-For-Testing/tests/QUnit_tests/integration.test.js
@@ -7,12 +7,31 @@ const user={
     gender:"male"
 } 
 
+let token=""
+
 QUnit.module("User functionalities", ()=> {
     QUnit.test("user registration returns correct user data", async(assert)=>{
           let path='/users/register'
           let random=Math.floor(Math.random()*1000)
           let randomEmail=`abv${random}@abv.bg`
-          let randomUserName=``    
+          let randomUserName=`Auto_Test_User_${random}` 
+          
+          
+          user.email=randomEmail;
+          user.userName=randomUserName
+          
+          let response=await fetch(basetUrl+path, {
+            method: "POST",
+            headers: {
+                "content-type":"application/json"
+            },
+            body: JSON.stringify(user)
+          }) 
+
+          let userData= await response.json();
+                    console.log(response) 
+
+          
 
     })
 })
