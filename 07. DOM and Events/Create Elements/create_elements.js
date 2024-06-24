4786e130a57b93fe1cad4cf7244f396ac3569e68
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-06-24 14:20:05 +0300

    files changed

--- 07. DOM and Events/Create Elements/create_elements.js
+++ 07. DOM and Events/Create Elements/create_elements.js
@@ -4,4 +4,18 @@ const movieItem=document.createElement('li')
 
 movieItem.textContent='Man'
 
-movieElements.appendChild(movieItem)
+movieElements.appendChild(movieItem) 
+
+//Create movie element with a link 
+
+const moviewithAtag=document.createElement('a')
+
+moviewithAtag.href='https://movie'
+moviewithAtag.textContent='The matrix'
+moviewithAtag.target='_blank' 
+
+const movieList=document.createElement('li')
+
+movieList.appendChild(moviewithAtag)
+
+movieElements.appendChild(movieList)
--- 08. DOM and Events exercise/Lab/01. List-Of-Items/app.js
+++ 08. DOM and Events exercise/Lab/01. List-Of-Items/app.js
@@ -1,20 +1,3 @@
 function addItem() {
-   // Get related element from Dom
-   const inputField=document.getElementById('newItemText')
-   const addList=document.getElementById('items') 
-  
-   //Create new element
-   const input=inputField.value
-   let newItem=document.createElement('li')
-   newItem.textContent=input
-   
-   //append new element
-   addList.append(newItem)
-
-   //clean value
-   inputField.value=''
-   
-   //focus on input 
-
-   inputField.focus()
+    
 }
