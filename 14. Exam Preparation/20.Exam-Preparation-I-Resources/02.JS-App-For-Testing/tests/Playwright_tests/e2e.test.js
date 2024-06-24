5dfac6036c8846d623f48f0e58ccd2d24ba07076
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-06-24 06:37:05 +0300

    files changed

--- 14. Exam Preparation/20.Exam-Preparation-I-Resources/02.JS-App-For-Testing/tests/Playwright_tests/e2e.test.js
+++ 14. Exam Preparation/20.Exam-Preparation-I-Resources/02.JS-App-For-Testing/tests/Playwright_tests/e2e.test.js
@@ -35,8 +35,85 @@ describe("e2e tests", () => {
     
     describe('testing authentication functionality ', async()=>{
         test('register with valid data returns correct API calls', async() =>{
-               page.goto(host)
-               
+              await page.goto(host)
+              await  page.click('text=Register')
+              await page.waitForSelector('form') 
+
+              let random=Math.floor(Math.random()*1000) 
+              let randomUserName=`RandomName_${random}`
+              let randomEmail=`abv${random}@abv.bg`
+              user.email=randomEmail
+              user.username=randomUserName
+              
+              await page.locator('#username').fill(user.username)
+              await page.locator('#email').fill(user.email)
+              await page.locator('#password').fill(user.password)
+              await page.locator('#repeatPass').fill(user.confirmPass)
+             
+
+              let [response] = await Promise.all ([
+                   page.waitForResponse(response=>response.url().includes('/users/register') && response.status()===200),
+                   page.click('[type="submit"]')
+              ]) 
+
+              expect(response.ok).toBeTruthy();
+
+              let userData=await response.json()
+              expect(userData.email).toBe(user.email)
+              expect(userData.password).toEqual(user.password)
+
+        })
+        test('login with valid credentials return correct API calls', async()=>{
+              await page.goto(host)
+              await page.click('text=Login')
+              await page.waitForSelector('form') 
+
+              await page.locator('#email').fill(user.email)
+              await page.locator('#password').fill(user.password)
+
+              let [response]=await Promise.all([
+                page.waitForResponse(response=>response.url().includes('/users/login')&&response.status()===200),
+                page.click('[type="submit"]')
+              ])
+
+              expect(response.ok).toBeTruthy()
+              let userData=await response.json();
+              expect(userData.email).toBe(user.email)
+              expect(userData.password).toBe(user.password)
+        })
+        test('logout  return correct API calls', async()=>{
+               await page.goto(host)
+               await page.click('text=Login')
+               await page.waitForSelector('form')
+
+               await page.locator('#email').fill(user.email)
+               await page.locator('#password').fill(user.password)
+               await page.click('[type="submit"]') 
+
+               let [response]=await Promise.all([
+                     page.waitForResponse(response=>response.url().includes('/users/logout')&&response.status()===204),
+                     page.click('text=Logout')
+               ])
+
+               expect(response.ok).toBeTruthy()
+
+               await page.waitForSelector('text=Login')
+
+               await page.waitForURL(host+'/')
+
+               expect(page.url()).toBe(host+'/')
+        })
+    })
+    describe('testing navBar functionallity', async()=>{
+        test('logged users sees correct buttons', async()=>{
+                page.goto(host)
+                page.click('text=Login')
+
+                page.locator('#email').click(user.email)
+                page.locator('#password').click(user.password)
+                page.click('[type="submit"]')
+
+                expect(page.locator('#All Memes')).toBeVisible();
         })
     })
     
