907a7ec6952a6cc520565c6e6284b9a8170eae2f
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-06-26 15:55:03 +0300

    files changed

--- 14. Exam Preparation/III/Resources/Task-1/app.js
+++ 14. Exam Preparation/III/Resources/Task-1/app.js
@@ -18,7 +18,10 @@ function solve() {
 
        addBtn.addEventListener('click', nextOn) 
 
-       function nextOn() {
+       function nextOn (event) {
+       
+        event.preventDefault()
+
         if(timeElement.value==""||
             dateElement.value==""||
             placeElement.value==""||
@@ -33,20 +36,17 @@ function solve() {
 
           let addArticleElement=document.createElement('article')
 
-          let timeParagraph=document.createElement('p')
-          timeParagraph.textContent=timeElement.value 
-
-          let dateParagraph=document.createElement('p')
-          dateParagraph.textContent=dateElement.value
+          let dateandTimeParagraph=document.createElement('p')
+          dateandTimeParagraph.textContent=`Begins: ${dateElement.value} at: ${timeElement.value}` 
 
           let placeParagraph=document.createElement('p')
-          placeParagraph.textContent=placeElement.value 
+          placeParagraph.textContent=`In: ${placeElement.value}` 
 
           let eventParagraph=document.createElement('p')
-          eventParagraph.textContent=eventElement.value 
+          eventParagraph.textContent=`Event: ${eventElement.value}` 
 
           let contactParagraph=document.createElement('p')
-          contactParagraph.textContent=contactsElement.value 
+          contactParagraph.textContent=`Contact: ${contactsElement.value}` 
 
           let editBtnElement=document.createElement('button')
           editBtnElement.setAttribute('class', 'edit-btn')
@@ -56,8 +56,7 @@ function solve() {
           continueBtnElement.setAttribute('class', 'continue-btn')
           continueBtnElement.textContent='Continue' 
 
-          addArticleElement.appendChild(timeParagraph)
-          addArticleElement.appendChild(dateParagraph)
+          addArticleElement.appendChild(dateandTimeParagraph)
           addArticleElement.appendChild(placeParagraph)
           addArticleElement.appendChild(eventParagraph)
           addArticleElement.appendChild(contactParagraph)
@@ -66,7 +65,89 @@ function solve() {
           liAddElement.appendChild(editBtnElement)
           liAddElement.appendChild(continueBtnElement) 
 
-          upcomingListElement.appendChild(liAddElement) 
+          lastCheckElement.appendChild(liAddElement) 
+
+          addBtn.disabled=true 
+
+          let editTimeElement=timeElement.value
+          let editDateElement=dateElement.value
+          let editPlaceElement=placeElement.value
+          let editEventElement=eventElement.value
+          let editContactElement=contactsElement.value
+
+          timeElement.value=""
+          dateElement.value=""
+          placeElement.value=""
+          eventElement.value=""
+          contactsElement.value=""
+
+          editBtnElement.addEventListener('click', editOn) 
+
+          function editOn () {
+              
+            timeElement.value=editTimeElement
+            dateElement.value=editDateElement
+            placeElement.value=editPlaceElement
+            eventElement.value=editEventElement
+            contactsElement.value=editContactElement
+            
+            liAddElement.remove()
+
+            addBtn.disabled=false
+
+          } 
+
+          continueBtnElement.addEventListener('click', continueOn) 
+
+          function continueOn () {
+               
+               let liContinueElement=document.createElement('li')
+               liContinueElement.setAttribute('class', 'event-content')
+
+               let articleContinueElement=document.createElement('article')
+               articleContinueElement=addArticleElement
+
+               let finishedBtnElement=document.createElement('button')
+               finishedBtnElement.setAttribute('class', 'finished-btn')
+               finishedBtnElement.textContent="Move to finished" 
+
+               liContinueElement.appendChild(articleContinueElement)
+               liContinueElement.appendChild(finishedBtnElement) 
+
+               upcomingListElement.appendChild(liContinueElement)
+
+               liAddElement.remove()
+
+               addBtn.disabled=false
+
+
+               finishedBtnElement.addEventListener('click', finishedOn) 
+
+               function finishedOn () {
+                   
+                   let liFinishedElement=document.createElement('li')
+                   liFinishedElement.setAttribute('class', 'event-content') 
+
+                   let articleFinishedElement=document.createElement('article') 
+
+                   articleFinishedElement=articleContinueElement 
+
+                   liFinishedElement.appendChild(articleFinishedElement)
+                   finishedListElement.appendChild(liFinishedElement)
+
+                   liContinueElement.remove()
+
+                   clearBth.addEventListener('click', clearOn)
+               
+               function clearOn () {
+                   liFinishedElement.remove()
+               
+                }
+                
+               }
+
+               
+          }
 
        }
 }
