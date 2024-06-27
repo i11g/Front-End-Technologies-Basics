0e673d1b8d0ae9e120a2f694ff62ce58226d64dc
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-06-27 14:55:22 +0300

    files changed

--- 14. Exam Preparation/I/20.Exam-Preparation-I-Resources/01.DOM-Manupulation/app.js
+++ 14. Exam Preparation/I/20.Exam-Preparation-I-Resources/01.DOM-Manupulation/app.js
@@ -1,5 +1,43 @@
 window.addEventListener("load", solve);
 
-function solve() {
-  
+function solve() {  
+
+    let snowManNameElement=document.getElementById('#snowman-name')
+    let heightElement=document.getElementById('#snowman-height') 
+    let locationElement=document.getElementById('#location') 
+    let creatorElement=document.getElementById('#creator-name') 
+    let specialAttributeElement=document.getElementById('#special-attribute') 
+
+    let btnAddElement=document.querySelector('[type="submit"]')
+    let snowManPreviewElement=document.querySelector('."snowman-preview"')
+    let yourSnowManElement=document.querySelector('.snow-list') 
+
+    let mainElement=documnet.getElementById('#hero')
+    let bodyElement=document.querySelector('.body')
+    let backImageEelement=document.getElementById('#back-img') 
+
+    btnAddElement.addEventListener('click', nextOn) 
+
+    function nextOn(event) {
+        event.preventDefault() 
+
+        if(snowManNameElement.value==""||
+            heightElement.value==""||
+            locationElement.value==""||
+            creatorElement.value==""||
+            specialAttributeElement==""
+        )
+        return
+    }   
+
+    let liElement=document.createElement('li')
+    liElement.setAttribute('class', 'snowman-info')
+    
+    let articleelement=document.createElement('article')
+    let nameParagraphElement=document.createElement('p')
+    nameParagraphElement.textContent=snowManNameElement.value
+    
+    let heightParagraph=document.createElement('p')
+    heightParagraph.textContent=heightElement.value
+
 }
