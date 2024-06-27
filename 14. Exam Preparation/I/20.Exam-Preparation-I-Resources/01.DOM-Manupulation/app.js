bb90e2d1d129ebbb61208bbe5e9087e4fecdea97
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-06-27 16:17:12 +0300

    files changed

--- 14. Exam Preparation/I/20.Exam-Preparation-I-Resources/01.DOM-Manupulation/app.js
+++ 14. Exam Preparation/I/20.Exam-Preparation-I-Resources/01.DOM-Manupulation/app.js
@@ -1,43 +1,102 @@
-window.addEventListener("load", solve);
+window.addEventListener('DOMContentLoaded', solve);
 
 function solve() {  
 
-    let snowManNameElement=document.getElementById('#snowman-name')
-    let heightElement=document.getElementById('#snowman-height') 
-    let locationElement=document.getElementById('#location') 
-    let creatorElement=document.getElementById('#creator-name') 
-    let specialAttributeElement=document.getElementById('#special-attribute') 
+    let snowElement=document.querySelector('#snowman-name')
+    console.log(snowElement)
+    let heightElement=document.getElementById("snowman-height") 
+    let locationElement=document.getElementById("location") 
+    let creatorElement=document.getElementById("creator-name") 
+    let specialAttributeElement=document.getElementById("special-attribute") 
 
     let btnAddElement=document.querySelector('[type="submit"]')
-    let snowManPreviewElement=document.querySelector('."snowman-preview"')
+    let snowManPreviewElement=document.querySelector('.snowman-preview')
     let yourSnowManElement=document.querySelector('.snow-list') 
 
-    let mainElement=documnet.getElementById('#hero')
+    let mainElement=document.getElementById('#hero')
     let bodyElement=document.querySelector('.body')
     let backImageEelement=document.getElementById('#back-img') 
 
     btnAddElement.addEventListener('click', nextOn) 
 
-    function nextOn(event) {
-        event.preventDefault() 
+    function nextOn(e) {
+        e.preventDefault() 
 
-        if(snowManNameElement.value==""||
+        if( snowElement.value ==""||
             heightElement.value==""||
             locationElement.value==""||
             creatorElement.value==""||
-            specialAttributeElement==""
-        )
-        return
-    }   
+            specialAttributeElement.value==""
+        ) {
+            return;
+        }   
 
-    let liElement=document.createElement('li')
-    liElement.setAttribute('class', 'snowman-info')
-    
-    let articleelement=document.createElement('article')
-    let nameParagraphElement=document.createElement('p')
-    nameParagraphElement.textContent=snowManNameElement.value
-    
-    let heightParagraph=document.createElement('p')
-    heightParagraph.textContent=heightElement.value
+            let liElement=document.createElement('li')
+            liElement.setAttribute('class', 'snowman-info')
+            
+            let articleElement=document.createElement('article')
 
-}
+            let nameParagraphElement=document.createElement('p')
+            nameParagraphElement.textContent=snowElement.value
+            
+            let heightParagraph=document.createElement('p')
+            heightParagraph.textContent=heightElement.value 
+
+            let locationParagraphElement=document.createElement('p')
+            locationParagraphElement.textContent=locationElement.value
+
+            let creatorParagraphElement=document.createElement('p')
+            creatorParagraphElement.textContent=creatorElement.value
+
+            let specialAtributeParagraph=document.createElement('p')
+            specialAtributeParagraph.textContent=specialAttributeElement.value 
+
+            let editBtnElement=document.createElement('button')
+            editBtnElement.setAttribute('class', 'edit-btn')
+            editBtnElement.textContent="Edit" 
+
+            let nextBtnElement=document.createElement('button')
+            nextBtnElement.setAttribute('class', 'next-btn')
+            nextBtnElement.textContent="Next" 
+
+        articleElement.appendChild(nameParagraphElement)
+        articleElement.appendChild(heightParagraph)
+        articleElement.appendChild(locationParagraphElement)
+        articleElement.appendChild(creatorParagraphElement)
+        articleElement.appendChild(specialAtributeParagraph) 
+
+        liElement.appendChild(articleElement)
+        liElement.appendChild(editBtnElement)
+        liElement.appendChild(nextBtnElement) 
+
+        snowManPreviewElement.appendChild(liElement)
+
+        btnAddElement.disabled=true  
+
+        let editName=snowElement.value;  
+        let editHeigh=heightElement.value
+        let editLocation=locationElement.value
+        let editCreator=creatorElement.value
+        let editSpecialAttribute=specialAttributeElement.value 
+
+        snowElement.value="";  
+        heightElement.value=""
+        locationElement.value=""
+        creatorElement.value=""
+        specialAttributeElement.value="" 
+        
+        editBtnElement.addEventListener('click', editOn)
+        function editOn() {
+            snowElement.value=editName;  
+            heightElement.value=editHeigh
+            locationElement.value=editLocation
+            creatorElement.value=editCreator
+            specialAttributeElement.value=editSpecialAttribute
+            
+            btnAddElement.disabled=false 
+
+            liElement.remove()
+        }
+    }
+} 
+
