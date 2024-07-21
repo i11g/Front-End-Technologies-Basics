f08511fade83c04e756b51dd752de402b32323de
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-07-21 18:40:22 +0300

    files changed.

--- 13. Playwright/15-Playwright-Introduction-Lab-Resources/tests/tests.spec.js
+++
@@ -1,7 +0,0 @@
-const {test, expect} =require('@playwright/test'); 
-
-test('Navigatong to Playwright', async({page})=> {
-    await page.goto('https://playwright.dev/')
-    const title=await page.title();
-    expect(title).toBe('Playwright')
-})
