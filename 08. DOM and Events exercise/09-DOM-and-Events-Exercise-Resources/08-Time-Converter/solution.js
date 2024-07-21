b3c0c08325d92e94ed19cdffec20eb0b63b07d94
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-07-21 14:45:01 +0300

    files changed.

--- 08. DOM and Events exercise/09-DOM-and-Events-Exercise-Resources/08-Time-Converter/solution.js
+++
@@ -1,9 +0,0 @@
-function attachEventsListeners() {
-    let dataToConvert=document.querySelector("input[type=text]")
-    let buttons=document. 
-
-    for (let i = 0; i < dataToConvert.length; i++) {
-       
-        
-    }
-}
