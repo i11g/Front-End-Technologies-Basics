eebb8313a8290c113ad778afa1a890505f0ea18c
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-08-15 18:27:37 +0300

    files changed

--- 14. Exam Preparation/Additional/Exam PrepII/Task-1/app.js
+++ 14. Exam Preparation/Additional/Exam PrepII/Task-1/app.js
@@ -2,7 +2,40 @@ window.addEventListener('load', solution);
 
 function solution() {
      
-       let 
+       let employeeElement=document.getElementById('employee')
+       let categoryElement=document.getElementById('category')
+       let urgencyElement=document.getElementById('urgency')
+       let teamElement=document.getElementById('team')
+       let descriptionElement=document.getElementById('description')
+       
+       let addButton=document.getElementById('add-btn')
+       
+       let previewContainer=document.querySelector('.preview-list')
+       let pendingContainer=document.querySelector('.pending-list')
+       let resolvedContainer=document.querySelector('.resolved-list')
+
+       addButton.addEventListener('Click', onNext)
+
+       function onNext(e) {
+        e.preventDefault();
+
+        if(employeeElement.value=="" ||
+          categoryElement.value==""||
+          urgencyElement.value==""||
+          teamElement.value==""||
+          descriptionElement.value==""
+
+        )  {
+           return ;
+        }
+
+        let liPreviewElement=document.createElement('li') 
+        liPreviewElement.setAttribute('class', 'problem-content') 
+        
+        let a
+
+
+       }
 }
 
 
